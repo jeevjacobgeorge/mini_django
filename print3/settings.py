9a5bab4201bd4bf679006eb83af001c136ece237
jeevjacobgeorge <jeev@J4Hp>
2024-05-06 15:19:25 +0530

    static url

--- print3/settings.py
+++ print3/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT ='/home/jeevjacobgeorge/miniprojectprintmanagement/static'
 
 
 # Default primary key field type
