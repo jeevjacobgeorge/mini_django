4da8a664f16c368324c2e48e21b3753aca463486
jeevjacobgeorge <jeev@J4Hp>
2024-05-06 16:25:46 +0530

    fixed

--- print3/settings.py
+++ print3/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
     "https://miniprojectprintmanagement.pythonanywhere.com"
+    "https://printmanagementfront-g2tkmzg25-jeevs-projects.vercel.app/",
 
 ]
 
