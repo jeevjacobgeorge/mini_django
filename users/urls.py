f10f61ca45bb1886fa7cb2c23689e00457e553df
jeevjacobgeorge <jeev@J4Hp>
2024-05-06 12:45:32 +0530

    added base path

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('signup', views.register, name="signup"),
     path('home', views.home, name="home"),
     path('home1', views.home1, name="home1"),
